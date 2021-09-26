bd435ad1bc3215269e73334b4da9da10ad435703
Nagu1244 <shabad.nagaraj@gmail.com>
2021-09-26 15:03:46 +0530

    Third commit

--- src/main/java/com/pma/org/entities/Employee.java
+++ src/main/java/com/pma/org/entities/Employee.java
@@ -24,7 +24,8 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 	private String email;
- //Code changed For GitHub Push
+	// Code changed For GitHub Push
+	//Thrird Commit
 	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
 			CascadeType.REFRESH })
 	@JoinTable(name = "project_employee", joinColumns = @JoinColumn(name = "employee_id"), inverseJoinColumns = @JoinColumn(name = "project_id"))
